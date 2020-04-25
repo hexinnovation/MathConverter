5330633335185d9a16f68da01f57110d32fa77d1
Tim Cooke <timothylcooke@gmail.com>
2020-04-25 05:20:20 -0400

    Add target and unit tests for xamarin.ios

---
+++ MathConverter.UnitTests/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using MSTestX;
+
+namespace MathConverter.UnitTests.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : TestRunnerApplicationDelegate
+    {
+    }
+}
